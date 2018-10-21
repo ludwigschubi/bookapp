b3b2df0982c6432d65282586acef4fb3e623b083
Markus Merklinger <markus.merklinger@code.berlin>
2018-10-21 17:18:56 +0200

    Fixed broken home of webapp root and readded template.

--- book_webapp/views.py
+++ book_webapp/views.py
@@ -1,8 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.contrib.auth.decorators import login_required
 
-@login_required()
 def webapp_home(request):
-    html = "<html><body>home_webapp</body></html>"
-    return HttpResponse(html)
+    return render(request, 'webapp_home.html')
