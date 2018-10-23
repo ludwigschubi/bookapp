a8df41e45f301db845ffca9425fb89a17906cbdd
Markus Merklinger <markus.merklinger@code.berlin>
2018-10-23 23:56:19 +0200

    Added example data for testing

---
+++ example_data/book_book.sql
@@ -0,0 +1,4 @@
+INSERT INTO book_book (isbn, title, author, price, owningCustomer_id) VALUES
+("978-3-89864-633-8", "Hacking mit Python", "Justin Seitz", 1, 1),
+("978-3-03831-103-4", "Gespräche mit Django dem Hund", "Friedrich Trepel", 1, 2),
+("978-3-7448-9713-6", "Monty Python", "Paul Fieter", 1, 1);
---
+++ example_data/book_customer.sql
@@ -0,0 +1,3 @@
+INSERT INTO book_customer (firstName, lastName, sex, street, streetNumber, postalCode, city, country_id, telephone, email) VALUES
+("Max", "Mustermann", 0, "Musterstraße", "1", 12345, "Musterstadt", 80, "030123456789", "max.mustermann@domain.de"),
+("Erika", "Musterfrau", 1, "Musterstraße", "1", 12345, "Musterstadt", 80, "030123456789", "erika.musterfrau@domain.de");
