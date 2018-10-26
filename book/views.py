1c0b39d1fb1b13fbc3fa6dcf32f86f2095ccdf65
Markus Merklinger <markus.merklinger@code.berlin>
2018-10-26 12:52:04 +0200

    Removed signup for local login

--- book/views.py
+++ book/views.py
@@ -1,19 +1,6 @@
 from django.shortcuts import render
 from django.template.loader import get_template
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-
-def signup(request):
-	if request.method == 'POST':
-		form = UserCreationForm(request.POST)
-		if form.is_valid():
-			# Save the user to the DB
-			form.save()
-			messages.success(request, 'Account created successfully')
-	else:
-		form = UserCreationForm()
-
-	return render(request, 'registration/signup.html', {'form': form})
 
 def gsignin(request):
 	return render(request, "g-signin.html")
