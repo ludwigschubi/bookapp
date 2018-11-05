2b5e3b486e2548bb22fd06cd7e2d96297db720dd
ludwigschubi <ludwigschubi1@gmail.com>
2018-11-05 19:54:49 +0100

    Tidy up home.js

--- static/js/home.js
+++ static/js/home.js
@@ -8,114 +8,6 @@ $('document').ready(function(){
     }, 10000)
 })
 
-let newBookFormString = `
-<div class="row">
-    <h3>Create a new book entry:</h3>
-</div>
-<div class="divider"></div>
-<div class="row">
-    <form class="col s10 offset-s1">
-        <div class="row">
-            <div class="input-field col s6">
-                <input type ="text" id="title">
-                <label for="title">Title</label>
-            </div>
-            <div class="input-field col s6">
-                <input type ="text" id="author">
-                <label for="author">Author</label>
-            </div>
-        </div>
-        <div class="row">
-            <div class="input-field col s6">
-                <input type ="text" id="isbn">
-                <label for="isbn">ISBN</label>
-            </div>
-            <div class="input-field col s6">
-                <input type ="text" id="cover">
-                <label for="cover">Cover</label>
-            </div>
-        </div>
-        <div class="row">
-            <div class="input-field col s12">
-                <input type ="number" step="0.1" id="price">
-                <label for="price">Price per day</label>
-            </div>
-        </div>
-    </form>
-</div>
-`
-
-$("#create-book").click(function() {
-    createBook(user.user)
-    loadBooks();
-});
-
-/** create/
-Allowed method: POST
-Required attributes: isbn, title, author, cover, price, owner*/
-
-function createBook(ownerId){
-    const bookData = {
-        isbn: $("#isbn").val(),
-        title: $("#title").val(),
-        author: $("#author").val(),
-        cover: $("#cover").val(),
-        price: $("#price").val(),
-        owner: ownerId,
-    }
-
-    var xhr = new XMLHttpRequest();
-    var url = "http://localhost:8000/api/book/create/";
-
-    xhr.responseType = "json";
-    xhr.onreadystatechange = () => {
-        if (xhr.readyState === XMLHttpRequest.DONE){
-          console.log("Book has been created!")  
-        };
-    };
-
-    xhr.open("POST", url);
-    xhr.setRequestHeader("Authorization", "Token " + token)
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.send(JSON.stringify(bookData));
-}
-
-function showUser(){
-    var xhr = new XMLHttpRequest();
-    var apiEndpoint = "http://localhost:8000/api/userAddress/show/";
-
-    xhr.responseType = "json";
-    xhr.onreadystatechange = () => {
-        if(xhr.readyState === XMLHttpRequest.DONE){
-            if(xhr.status == 200){
-                user = xhr.response;
-            }
-        }
-    };
-
-    xhr.open("GET", apiEndpoint);
-    xhr.setRequestHeader("Authorization", "Token " + token)
-    xhr.send();
-}
-
-function loadBooks(){
-    var xhr = new XMLHttpRequest();
-    var apiEndpoint = "http://localhost:8000/api/book/list/";
-
-    xhr.responseType = "json";
-    xhr.onreadystatechange = () => {
-        if(xhr.readyState === XMLHttpRequest.DONE){
-            if(xhr.status == 200){
-                displayBooks(xhr.response);
-            }
-        }
-    };
-
-    xhr.open("GET", apiEndpoint);
-    xhr.setRequestHeader("Authorization", "Token " + token)
-    xhr.send();
-}
-
 /***
  *    ███████╗██╗L██████╗L███╗LLL██╗██╗LLL██╗██████╗L
  *    ██╔════╝██║██╔════╝L████╗LL██║██║LLL██║██╔══██╗
@@ -204,6 +96,116 @@ $("#loginButton").click(function() {
 });
 
 /***
+ *    ██╗LLL██╗███████╗███████╗██████╗L██████╗L██╗███████╗██████╗L██╗LLLLLL█████╗L██╗LLL██╗
+ *    ██║LLL██║██╔════╝██╔════╝██╔══██╗██╔══██╗██║██╔════╝██╔══██╗██║LLLLL██╔══██╗╚██╗L██╔╝
+ *    ██║LLL██║███████╗█████╗LL██████╔╝██║LL██║██║███████╗██████╔╝██║LLLLL███████║L╚████╔╝L
+ *    ██║LLL██║╚════██║██╔══╝LL██╔══██╗██║LL██║██║╚════██║██╔═══╝L██║LLLLL██╔══██║LL╚██╔╝LL
+ *    ╚██████╔╝███████║███████╗██║LL██║██████╔╝██║███████║██║LLLLL███████╗██║LL██║LLL██║LLL
+ *    L╚═════╝L╚══════╝╚══════╝╚═╝LL╚═╝╚═════╝L╚═╝╚══════╝╚═╝LLLLL╚══════╝╚═╝LL╚═╝LLL╚═╝LLL
+ *    LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL
+ */
+
+function showUser(){
+    var xhr = new XMLHttpRequest();
+    var apiEndpoint = "http://localhost:8000/api/userAddress/show/";
+
+    xhr.responseType = "json";
+    xhr.onreadystatechange = () => {
+        if(xhr.readyState === XMLHttpRequest.DONE){
+            if(xhr.status == 200){
+                user = xhr.response;
+            }
+        }
+    };
+
+    xhr.open("GET", apiEndpoint);
+    xhr.setRequestHeader("Authorization", "Token " + token)
+    xhr.send();
+}
+
+/***
+ *    ██████╗LL██████╗LL██████╗L██╗LL██╗L██████╗██████╗L███████╗L█████╗L████████╗██╗L██████╗L███╗LLL██╗
+ *    ██╔══██╗██╔═══██╗██╔═══██╗██║L██╔╝██╔════╝██╔══██╗██╔════╝██╔══██╗╚══██╔══╝██║██╔═══██╗████╗LL██║
+ *    ██████╔╝██║LLL██║██║LLL██║█████╔╝L██║LLLLL██████╔╝█████╗LL███████║LLL██║LLL██║██║LLL██║██╔██╗L██║
+ *    ██╔══██╗██║LLL██║██║LLL██║██╔═██╗L██║LLLLL██╔══██╗██╔══╝LL██╔══██║LLL██║LLL██║██║LLL██║██║╚██╗██║
+ *    ██████╔╝╚██████╔╝╚██████╔╝██║LL██╗╚██████╗██║LL██║███████╗██║LL██║LLL██║LLL██║╚██████╔╝██║L╚████║
+ *    ╚═════╝LL╚═════╝LL╚═════╝L╚═╝LL╚═╝L╚═════╝╚═╝LL╚═╝╚══════╝╚═╝LL╚═╝LLL╚═╝LLL╚═╝L╚═════╝L╚═╝LL╚═══╝
+ *    LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL
+ */
+
+let newBookFormString = `
+<div class="row">
+    <h3>Create a new book entry:</h3>
+</div>
+<div class="divider"></div>
+<div class="row">
+    <form class="col s10 offset-s1">
+        <div class="row">
+            <div class="input-field col s6">
+                <input type ="text" id="title">
+                <label for="title">Title</label>
+            </div>
+            <div class="input-field col s6">
+                <input type ="text" id="author">
+                <label for="author">Author</label>
+            </div>
+        </div>
+        <div class="row">
+            <div class="input-field col s6">
+                <input type ="text" id="isbn">
+                <label for="isbn">ISBN</label>
+            </div>
+            <div class="input-field col s6">
+                <input type ="text" id="cover">
+                <label for="cover">Cover</label>
+            </div>
+        </div>
+        <div class="row">
+            <div class="input-field col s12">
+                <input type ="number" step="0.1" id="price">
+                <label for="price">Price per day</label>
+            </div>
+        </div>
+    </form>
+</div>
+`
+
+$("#create-book").click(function() {
+    createBook(user.user)
+    loadBooks();
+});
+
+/** create/
+Allowed method: POST
+Required attributes: isbn, title, author, cover, price, owner*/
+
+function createBook(ownerId){
+    const bookData = {
+        isbn: $("#isbn").val(),
+        title: $("#title").val(),
+        author: $("#author").val(),
+        cover: $("#cover").val(),
+        price: $("#price").val(),
+        owner: ownerId,
+    }
+
+    var xhr = new XMLHttpRequest();
+    var url = "http://localhost:8000/api/book/create/";
+
+    xhr.responseType = "json";
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === XMLHttpRequest.DONE){
+          console.log("Book has been created!")  
+        };
+    };
+
+    xhr.open("POST", url);
+    xhr.setRequestHeader("Authorization", "Token " + token)
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(JSON.stringify(bookData));
+}
+
+/***
  *    ██████╗LL██████╗LL██████╗L██╗LL██╗██████╗L██╗███████╗██████╗L██╗LLLLLL█████╗L██╗LLL██╗
  *    ██╔══██╗██╔═══██╗██╔═══██╗██║L██╔╝██╔══██╗██║██╔════╝██╔══██╗██║LLLLL██╔══██╗╚██╗L██╔╝
  *    ██████╔╝██║LLL██║██║LLL██║█████╔╝L██║LL██║██║███████╗██████╔╝██║LLLLL███████║L╚████╔╝L
@@ -212,6 +214,24 @@ $("#loginButton").click(function() {
  *    ╚═════╝LL╚═════╝LL╚═════╝L╚═╝LL╚═╝╚═════╝L╚═╝╚══════╝╚═╝LLLLL╚══════╝╚═╝LL╚═╝LLL╚═╝LLL
  *    LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL
  */
+
+function loadBooks(){
+    var xhr = new XMLHttpRequest();
+    var apiEndpoint = "http://localhost:8000/api/book/list/";
+
+    xhr.responseType = "json";
+    xhr.onreadystatechange = () => {
+        if(xhr.readyState === XMLHttpRequest.DONE){
+            if(xhr.status == 200){
+                displayBooks(xhr.response);
+            }
+        }
+    };
+
+    xhr.open("GET", apiEndpoint);
+    xhr.setRequestHeader("Authorization", "Token " + token)
+    xhr.send();
+}
 
 function displayBooks(fetchedBooks){
     $("#bookContainer").empty();
