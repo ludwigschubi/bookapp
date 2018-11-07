36dfd10190cb9b9897665c10b37921865a36328e
Markus Merklinger <markus.merklinger@code.berlin>
2018-11-07 15:25:25 +0100

    Fixes cover field data to validate correct in the model

--- example_data/book_book.sql
+++ example_data/book_book.sql
@@ -1,4 +1,4 @@
 INSERT INTO book_book (isbn, title, author, cover, price, owner_id) VALUES
-('978-3-89864-633-8', 'Hacking mit Python', 'Justin Seitz', 'www.bilder.de', 1, 1),
-('978-3-03831-103-4', 'Gespräche mit Django dem Hund', 'Friedrich Trepel', 'www.bilder.de', 1, 2),
-('978-3-7448-9713-6', 'Monty Python', 'Paul Fieter', 'www.bilder.de', 1, 1);
+('978-3-89864-633-8', 'Hacking mit Python', 'Justin Seitz', 'http://www.bilder.de', 1, 1),
+('978-3-03831-103-4', 'Gespräche mit Django dem Hund', 'Friedrich Trepel', 'http://www.bilder.de', 1, 2),
+('978-3-7448-9713-6', 'Monty Python', 'Paul Fieter', 'http://www.bilder.de', 1, 1);
