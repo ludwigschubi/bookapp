16b6c9dbaf8a904804c86231539d239634188da8
Markus Merklinger <markus.merklinger@code.berlin>
2019-02-04 15:21:06 +0100

    Removes not required example data

--- example_data/book_bookcategory.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO book_bookcategory (name) VALUES ('General');
-INSERT INTO book_bookcategory (name) VALUES ('STS');
-INSERT INTO book_bookcategory (name) VALUES ('SE');
-INSERT INTO book_bookcategory (name) VALUES ('PM');
-INSERT INTO book_bookcategory (name) VALUES ('ID');
